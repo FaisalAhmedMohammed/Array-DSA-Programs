f8351582b0f825a575d5cc4cc99971929628b4d0
Faisal Ahmed <mohammedfaisalahmed873@gmail.com>
2026-03-13 00:45:14 +0530

    added logic when array is null

--- LinearSearchAlgorithm/_06_FInd_Maximum_Element.java
+++ LinearSearchAlgorithm/_06_FInd_Maximum_Element.java
@@ -4,20 +4,22 @@ public class _06_FInd_Maximum_Element {
 
 	public static void main(String[] args) {
 
-		int[] arr = { 5, 5 };
+		int[] arr = { 2, 1, 4, 6, 7, 8 }; // here if arr = null then we will get NullPointerException
 
 		int max2 = findMax(arr);
-		System.out.println("Maximum element is: " + max2);
+
+		if (max2 != -1) {
+			System.out.println("Maximum element is: " + max2);
+		}
 	}
 
 	private static int findMax(int[] arr) {
 
-		int max = arr[0];
-
-		if (arr.length == 0) {
+		if (arr == null || arr.length == 0) {
 			System.out.println("Array is empty");
 			return -1;
 		}
+		int max = arr[0];
 
 		for (int i = 1; i < arr.length; i++) {
 
