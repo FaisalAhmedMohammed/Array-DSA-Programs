f33cded7325f94fa7dbc515224730cadb364c08e
Faisal Ahmed <mohammedfaisalahmed873@gmail.com>
2026-03-14 16:51:19 +0530

    added last occurrennce of element

--- BinarySearchAlgorithm/_05_Print_Last_Occurance_Of_Element.java
+++ BinarySearchAlgorithm/_05_Print_Last_Occurance_Of_Element.java
@@ -5,14 +5,14 @@ public class _05_Print_Last_Occurance_Of_Element {
 	public static void main(String[] args) {
 
 		int[] arr = { 2, 4, 6, 8, 8, 8, 10, 12 };
-
 		int target = 8;
 
 		int last = findLast(arr, target);
-		System.out.println(last);
+
+		System.out.println("Last Occurrence Index : " + last);
 	}
 
-	private static int findLast(int[] arr, int target) {
+	public static int findLast(int[] arr, int target) {
 
 		int start = 0;
 		int end = arr.length - 1;
@@ -20,21 +20,18 @@ public class _05_Print_Last_Occurance_Of_Element {
 
 		while (start <= end) {
 
-			int mid = (start + end) / 2;
+			int mid = start + (end - start) / 2;
 
 			if (arr[mid] == target) {
-
 				result = mid;
-				start = mid + 1;
-			}
-
-			else if (arr[mid] < target) {
+				start = mid + 1; // move right
+			} else if (arr[mid] < target) {
 				start = mid + 1;
 			} else {
 				end = mid - 1;
 			}
 		}
+
 		return result;
 	}
-
 }
